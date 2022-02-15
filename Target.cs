7718cdc99585796cff93be962264816d3fcaf012
theanh232 <99399457+theanh232@users.noreply.github.com>
2022-02-15 17:36:31 +0700

    Add files via upload

---
+++ Target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Rigidbody targetRb;
+
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(Vector3.up*Random.Range(12,16),ForceMode.Impulse); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
