e5b5317dd0c12caf91f4bd16fa7772a5309d3a54
theanh232 <theanh020597@gmai.com>
2022-02-16 14:35:52 +0700

    first commit

---
+++ Assets/Course Library/Script/Gamemanagers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanagers : MonoBehaviour
+{
+    public List<GameObject> target;
+   private float spawnRate = 1.0f;
+    void Start()
+
+    {
+
+        StartCoroutine(SqawnTarget());
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    IEnumerator  SqawnTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, target.Count);
+            Instantiate(target[index]);
+
+        }
+
+    }
+}
---
+++ Assets/Course Library/Script/Target.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Rigidbody targetRb;
+
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(Vector3.up*Random.Range(12,16),ForceMode.Impulse);
+
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
+
+        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10,10), Random.Range(-10, 10),ForceMode.Impulse);
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        destroygameobject();
+    }
+    public void destroygameobject()
+    {
+        if(transform.position.y < -16 )
+        {
+            Destroy(gameObject);
+        }    
+    }    
+}
